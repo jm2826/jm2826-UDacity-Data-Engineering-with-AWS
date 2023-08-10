7fa7bf813f2ce9d54c3c25079bec2626d40238cf
Joel Magee <joelemagee@gmail.com>
2023-08-10 14:47:59 -0500

    Add files via upload

---
+++ Data Piplines/Airflow and AWS Errors/l3_e4_s3_to_redshift.py
@@ -0,0 +1,50 @@
+import pendulum
+import logging
+
+from airflow.decorators import dag, task
+from airflow.secrets.metastore import MetastoreBackend
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.operators.postgres_operator import PostgresOperator
+from airflow.operators.python_operator import PythonOperator
+
+from udacity.common import sql_statements
+
+@dag(
+    start_date=pendulum.now()
+)
+def load_data_to_redshift():
+#def s3_to_redshift_legacy():
+
+    @task
+    def load_task():    
+        metastoreBackend = MetastoreBackend()
+        aws_connection=metastoreBackend.get_connection("aws_credentials")
+        redshift_hook = PostgresHook("redshift")
+        redshift_hook.run(sql_statements.COPY_ALL_TRIPS_SQL.format(aws_connection.login, aws_connection.password))
+
+    create_table_task=PostgresOperator(
+       task_id="create_table",
+        postgres_conn_id="redshift",
+        sql=sql_statements.CREATE_TRIPS_TABLE_SQL
+    )
+
+    drop_location_traffic_task = PostgresOperator(
+        task_id="drop_calculate_location_traffic",
+        postgres_conn_id="redshift",
+        sql=sql_statements.DROP_LOCATION_TRAFFIC_SQL
+    )
+
+    location_traffic_task = PostgresOperator(
+        task_id="calculate_location_traffic",
+        postgres_conn_id="redshift",
+        sql=sql_statements.LOCATION_TRAFFIC_SQL
+    )
+
+    load_data = load_task()
+    create_table_task >> load_data
+    load_data >> drop_location_traffic_task
+    drop_location_traffic_task >> location_traffic_task
+
+#s3_to_redshift_dag = load_data_to_redshift()
+s3_to_redshift_legacy = load_data_to_redshift()
+
